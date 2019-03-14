892771fb2ff91ae734597fbbbce5f34858445842
Junior Santos <jddsjr98@gmail.com>
2019-03-14 20:04:58 -0300

    add new Articles keys

--- src/Reference.cs
+++ src/Reference.cs
@@ -8,23 +8,56 @@ namespace csvToBib.src
             this.id = id;
                
         }
-                                public int id { get; set; }
+        public int id { get; set; }
+
         static TypeIEEERef misc = new TypeIEEERef(0, "", "misc");
         static TypeIEEERef inproceedings = new TypeIEEERef(1, "IEEE Conferences", "INPROCEEDINGS");
         TypeIEEERef article = new TypeIEEERef(2, "IBM Journals & Magazines", "ARTICLE");
         TypeIEEERef Article = new TypeIEEERef(2, "IEEE Early Access Articles", "ARTICLE");
         // TypeIEEERef INBOOK = new TypeIEEERef(1, "IEEE Early Access Articles", "INBOOK");
         
-
+        
         
         public Reference(IEEEfile ieeeFile){
             var type = identifierType(ieeeFile);
-            base.Type = type.typeBib;
-            base.Title = ieeeFile.DocumentTitle;
-            base.Year = ieeeFile.Publication_Year;
-            base.Publisher = ieeeFile.Publisher;
-            base.Author = ieeeFile.Authors;
-            base.Key = this.id.ToString();
+            if(type.Equals(inproceedings)){
+                base.Author = ieeeFile.Authors;
+                base.Title = ieeeFile.DocumentTitle;
+                base.Booktitle = ieeeFile.PublicationTitle;
+                base.Type = type.typeBib;
+                base.Year = ieeeFile.Publication_Year;
+                base.Volume = ieeeFile.Volume;
+                base.Pages = $"{ieeeFile.StartPage} - {ieeeFile.EndPage}";
+                this["keywords"] = $"{ieeeFile.INSPECControlledTerms}{ieeeFile.INSPECNonControlledTerms}{ieeeFile.IEEETerms}";
+                this["doi"] = ieeeFile.DOI;
+                base.Key = this.id.ToString();
+                
+                Console.WriteLine("é inproceedings");
+            }else if(type.Equals(article) || type.Equals(Article)){
+                Console.WriteLine("é article");
+
+
+                base.Author = ieeeFile.Authors;
+                base.Journal = ieeeFile.PublicationTitle;
+                base.Title = ieeeFile.DocumentTitle;
+                base.Year = ieeeFile.Publication_Year;
+                base.Volume = ieeeFile.Volume;
+                base.Pages = $"{ieeeFile.StartPage} - {ieeeFile.EndPage}";
+
+                base.Key = this.id.ToString();
+                
+            }else{
+                Console.WriteLine("é misc");
+
+                base.Type = type.typeBib;
+                base.Title = ieeeFile.DocumentTitle;
+                base.Year = ieeeFile.Publication_Year;
+                base.Publisher = ieeeFile.Publisher;
+                base.Author = ieeeFile.Authors;
+                base.Key = this.id.ToString();
+                
+            }
+            
         }
         private TypeIEEERef identifierType(IEEEfile ieeefile){
             switch (ieeefile.DocumentIdentifier)
@@ -32,6 +65,12 @@ namespace csvToBib.src
                 case "IEEE Conferences":
                     return inproceedings;
                 case "IBM Journals & Magazines":
+                    return  article;
+                case "IET Journals & Magazines":
+                    return  article;
+                case "IEEE Journals & Magazines":
+                    return  article;
+                case "OUP Journals & Magazines":
                     return  article;
                 case "IEEE Early Access Articles":
                     return Article;
